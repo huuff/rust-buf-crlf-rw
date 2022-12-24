5689268855d15e129fbeaf49f65499146e7d48ce
Haf <haf@protonmail.ch>
2022-12-24 17:19:42 +0100

    hopefully finished implementation

--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,24 @@ impl<T: Read> ReadCrlfLine for BufReader<T> {
         }
 
         let mut consumed = 0;
-        while fill_buf[consumed] != b'\r' && fill_buf[consumed+1] != b'\n' {
-           consumed += 1; 
+        // XXX: Spaghetti conditional
+        while consumed < fill_buf.len() // Buffer is not finished
+            && (consumed == fill_buf.len()-1 // If it's the end of the buffer, just finish reading
+                                             // it
+            || (fill_buf[consumed] != b'\r' && fill_buf[consumed+1] != b'\n')) // Ensure CRLF
+                                                                               // hasn't been found
+                                                                               // yet 
+        {
+            consumed += 1; 
         }
 
         // TODO: Please do not unwrap
         buffer.push_str(str::from_utf8(&fill_buf[..consumed]).unwrap());
 
-        consumed += 2;
+        if consumed < fill_buf.len() {
+            // Found a CRLF before the buffer ends, so we manually consume it
+            consumed += 2;
+        }
         self.consume(consumed);
 
         return Ok(consumed);
@@ -38,7 +48,7 @@ mod tests {
         let mut line = String::with_capacity(15);
         let mut buf_reader = BufReader::new(
             "This is a text\r\nwith two lines".as_bytes()
-        );
+            );
 
         // ACT
         let result = buf_reader.read_crlf_line(&mut line);
@@ -50,11 +60,10 @@ mod tests {
 
     #[test]
     fn two_lines() {
-        // ARRANGE
         let mut line = String::with_capacity(32);
         let mut buf_reader = BufReader::new(
             "This is a text\r\nwith three lines\r\nseparated by crlf".as_bytes()
-        );
+            );
 
         let first_result = buf_reader.read_crlf_line(&mut line);
         assert_eq!(line, "This is a text");
@@ -65,5 +74,16 @@ mod tests {
         let second_result = buf_reader.read_crlf_line(&mut line);
         assert_eq!(line, "with three lines");
         assert_eq!(second_result.unwrap(), 18);
+    }
+
+    #[test]
+    fn reads_to_end_when_theres_no_crlf() {
+        let mut line = String::with_capacity(32);
+        let mut buf_reader = BufReader::new("this is a line with no crlf".as_bytes());
+
+        let result = buf_reader.read_crlf_line(&mut line);
+
+        assert_eq!(line, "this is a line with no crlf");
+        assert_eq!(result.unwrap(), 27);
     }
 }
